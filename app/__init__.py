0205720131046c68973572469487726ae6e72dd1
kyle strougo <kstrougo@gmail.com>
2023-12-13 16:34:02 -0500

    cleaned masters E

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,6 @@ from flask_moment import Moment
 from flask_mail import Mail
 from flask_apscheduler import APScheduler
 
-
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
@@ -19,9 +18,34 @@ moment = Moment(app)
 app.config['STATIC_FOLDER'] = 'static'
 mail = Mail(app)
 scheduler = APScheduler()
-scheduler.init_app(app)
+##scheduler.init_app(app)
+
+
 # to  update with prod URL  domain?
 ##app.config['SESSION_COOKIE_DOMAIN'] = 'localhost.localdomain'
 
 from app import routes, models
+from app.routes import tournament_start
+from helper import update_data, send_leaderboard_email
+from datetime import datetime
+import os
 
+def add_jobs():
+    scheduler.add_job(id='refresh_scores', func=update_data, trigger='interval', seconds=30)
+
+    start_date = datetime(2023, 12, 13, 13, 41, 0)
+    end_date = datetime(2023, 12, 25, 17, 7, 30)
+
+    trigger = CronTrigger(hour=17, minute=23, second=0)
+    scheduler.add_job(id='email_leaderboard', func=send_leaderboard_email, trigger=trigger, start_date=start_date,
+                      end_date=end_date)
+
+    scheduler.add_job(id='grant_access', func=tournament_start, run_date=start_date)
+    scheduler.add_job(id='revoke_access', func=tournament_start, run_date=start_date)
+
+
+def initialize_scheduler():
+    if os.environ.get('WERKZEUG_RUN_MAIN') != 'true':
+        scheduler.init_app(app)
+        add_jobs()
+        scheduler.start()
--- main.py
+++ main.py
@@ -1,29 +1,32 @@
-from app import app, scheduler
+from app import app, initialize_scheduler,scheduler
 from helper import update_data, send_leaderboard_email
 from app.routes import tournament_start
 from apscheduler.triggers.cron import CronTrigger
 from datetime import datetime
+'''
+# ----------------- Tournament Dates -----------------
+start_date = datetime(2023, 12, 13, 13, 41, 0)
+end_date = datetime(2023, 12, 25, 17, 7, 30)
 
+# ----------------- Masters data every 30 seconds -----------------
+scheduler.add_job(id='refresh_scores', func=update_data, trigger='interval', seconds=10)
+
+# ----------------- Leaderboard Report every EOD -----------------
+trigger = CronTrigger(hour=17, minute=23, second=0)
+scheduler.add_job(id='email_leaderboard', func=send_leaderboard_email, trigger=trigger, start_date=start_date,
+                  end_date=end_date)
+
+# ----------------- Upon tournament start, give access to other user profiles -----------------
+scheduler.add_job(id='grant_access', func=tournament_start, run_date=start_date)
+
+# ----------------- Revoke Drafting Capabilities at -----------------
+scheduler.add_job(id='revoke_access', func=tournament_start, run_date=start_date)
+
+'''
 
 if __name__ == '__main__':
-    # ----------------- Tournament Dates -----------------
-    start_date = datetime(2023, 12, 10, 17, 2, 40)
-    end_date = datetime(2023, 12, 25, 17, 7, 30)
-
-    # ----------------- Masters data every 30 seconds -----------------
-    scheduler.add_job(id='masters_data', func=update_data, trigger='interval', seconds=30)
-
-    # ----------------- Leaderboard Report every EOD -----------------
-    trigger = CronTrigger(hour=17, minute=23, second=0)
-    scheduler.add_job(id='email_leaderboard', func=send_leaderboard_email, trigger=trigger, start_date=start_date,end_date=end_date)
-
-    # ----------------- Upon tournament start, give access to other user profiles -----------------
-    scheduler.add_job(id='grant_access_to_other_user_data', func=tournament_start, run_date=start_date)
-
-    # ----------------- Revoke Drafting Capabilities at -----------------
-    scheduler.add_job(id='revoke_access_to_drafting', func=tournament_start, run_date=start_date)
-
     # ----------------- Start Jobs, Start App -----------------
-    scheduler.start()
+    ##scheduler.start()
+    initialize_scheduler()
 
     app.run(debug=True, host='0.0.0.0')
