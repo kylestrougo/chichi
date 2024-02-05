e91bc92421a1b636c88cfc098ed511771a1d704a
kyle strougo <kstrougo@gmail.com>
2024-02-04 19:00:57 -0500

    configured for Phoenix

--- main.py
+++ main.py
@@ -14,22 +14,22 @@ def add_jobs():
 
     #day1 dates
     end_dt_d1 = datetime(2024, 2, 8, 22, 0, 0)
-    scheduler.add_job(id='refresh_scores', func=update_data, trigger='interval', minutes=30, start_date=t_start, end_date=end_dt_d1)
+    scheduler.add_job(id='refresh_scores1', func=update_data, trigger='interval', minutes=30, start_date=t_start, end_date=end_dt_d1)
 
     #day2 dates
     start_dt_d2 = datetime(2024, 2, 9, 9, 0, 0)
     end_dt_d2 = datetime(2024, 2, 9, 22, 0, 0)
-    scheduler.add_job(id='refresh_scores', func=update_data, trigger='interval', minutes=10, start_date=start_dt_d2, end_date=end_dt_d2)
+    scheduler.add_job(id='refresh_scores2', func=update_data, trigger='interval', minutes=10, start_date=start_dt_d2, end_date=end_dt_d2)
 
     #day3 dates
     start_dt_d3 = datetime(2024, 2, 10, 9, 0, 0)
     end_dt_d3 = datetime(2024, 2, 10, 22, 0, 0)
-    scheduler.add_job(id='refresh_scores', func=update_data, trigger='interval', minutes=5, start_date=start_dt_d3, end_date=end_dt_d3)
+    scheduler.add_job(id='refresh_scores3', func=update_data, trigger='interval', minutes=5, start_date=start_dt_d3, end_date=end_dt_d3)
 
     #day4 dates
     start_dt_d4 = datetime(2024, 2, 11, 9, 0, 0)
     end_dt_d4 = datetime(2024, 2, 11, 22, 0, 0)
-    scheduler.add_job(id='refresh_scores', func=update_data, trigger='interval', minutes=2, start_date=start_dt_d4, end_date=end_dt_d4)
+    scheduler.add_job(id='refresh_scores4', func=update_data, trigger='interval', minutes=2, start_date=start_dt_d4, end_date=end_dt_d4)
 
     email_trigger = CronTrigger(hour=20, minute=0, second=0)
     scheduler.add_job(id='email_leaderboard', func=send_leaderboard_email, trigger=email_trigger, start_date=t_start,
