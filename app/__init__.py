18408cdd89989fb1c9e26b0d702f426db6f0664b
kyle strougo <kstrougo@gmail.com>
2024-03-24 17:59:01 -0400

    deploy dev server

--- app/__init__.py
+++ app/__init__.py
@@ -18,8 +18,8 @@ moment = Moment(app)
 app.config['STATIC_FOLDER'] = 'static'
 mail = Mail(app)
 # to  update with prod URL  domain?
-app.config['SESSION_COOKIE_DOMAIN'] = 'chi-chi.duckdns.org'
-app.config['REMEMBER_COOKIE_DOMAIN'] = '.chi-chi.duckdns.org'
+#app.config['SESSION_COOKIE_DOMAIN'] = 'chi-chi.duckdns.org'
+#app.config['REMEMBER_COOKIE_DOMAIN'] = '.chi-chi.duckdns.org'
 app.config['SESSION_COOKIE_SECURE'] = False
 app.config['REMEMBER_COOKIE_SECURE'] = False
 app.config['SECRET_KEY'] = 'qwert54321'
